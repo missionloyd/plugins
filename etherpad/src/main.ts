32f1b81e55d55627b6712e8a4312455f0fddedec
renambot <renambot@gmail.com>
2023-11-15 15:12:18 -0600

    add user

--- etherpad/src/main.ts
+++ etherpad/src/main.ts
@@ -9,12 +9,17 @@ type EtherpadState = {
 const s3api = new SAGE3Plugin<EtherpadState>();
 
 // Subscribe to updates from the SAGE3 server when other clients update the state.
-s3api.subscribeToUpdates((state) => {
-  console.log("state", state);
+s3api.subscribeToUpdates((state: Plugin<EtherpadState>, user: string) => {
+  console.log("Etherpad state>", state);
   const uuid = state._id;
 
-  // &userName=sage
-  const url = `https://etherpad.nrp-nautilus.io/p/sage3-${uuid}?showChat=false&showLineNumbers=true&useMonospaceFont=false&noColors=false`;
+  let url: string;
+  if (user) {
+    const username = encodeURIComponent(user);
+    url = `https://etherpad.nrp-nautilus.io/p/sage3-${uuid}?showChat=false&showLineNumbers=true&useMonospaceFont=false&noColors=false&userName=${username}`;
+  } else {
+    url = `https://etherpad.nrp-nautilus.io/p/sage3-${uuid}?showChat=false&showLineNumbers=true&useMonospaceFont=false&noColors=false`;
+  }
 
   // Save the URL to the state, just in case
   s3api.update({ state: { url: url } });
